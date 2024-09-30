504117dcaa2b7f478a6953b091123962f86c2b34
Matthew Wright <matthewbwright@gmail.com>
2024-09-30 14:02:39 -0700

    sets up GET ALL PRODUCTS endpoint

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,14 @@ const app = express();
 
 app.use(express.json()) //allows u sto accept JSON data in the req.body
 
-app.get("/api/products", (req,res) => {
-    res.send("Server is ready")
+app.get("/api/products", async (req,res) => {
+    try {
+        const products = await Product.find({})
+        res.status(200).json({success: true, data: products})
+    } catch(error) {
+        console.log("error in fetching products:", error.message)
+        res.status(500).json({success: false, message: "Server Error"})
+    }
 })
 
 app.post("/api/products", async (req, res) => {
@@ -40,6 +46,7 @@ app.delete("/api/products/:id", async (req, res) => {
         res.status(200).json({success: true, message: "Product deleted"
         })
     } catch (error) {
+        console.log("error in deleting product:", error.message)
         res.status(404).json({success:false, message: "Product not found"})
     }
 })
